482151f0da0a6f88d337b0fdbd39e39df94b3b33
jcarrabino <jcarrabino13@gmail.com>
2017-03-02 12:15:27 -0500

    Add files via upload

--- code/bot.js
+++ code/bot.js
@@ -8,78 +8,98 @@ var MsTranslator = require('mstranslator');
 var MSconfig = require('./MSkey.js');
 var client = new MsTranslator(MSconfig, true);
 
-//Set up Node-Scheduler for Automation
+//Set up Node Schedule
 var schedule = require('node-schedule');
 
-//Set rules for retweet schedule
-var rule = new schedule.RecurrenceRule();
-rule.minute = 10;
-rule.minute = 5;
-rule.minute = 30;
-rule.minute = 45;
+//Set up Timestamp
+var timestamp = require('console-timestamp');
 
-//Call on retweet function based on scheduler's rules
-var trollBot = schedule.scheduleJob(rule, function(){
+//Automate bot to reply to tweet's based on scheduler's rules
+var trollBot = schedule.scheduleJob('*/30 * * * *', function(){
     // find latest tweet according the query 'q' in params
     var params = {
         q: 'from:realDonaldTrump',  // REQUIRED
         result_type: 'recent',
-        count: '5'
+        count: '1'
     }
     // for more parametes, see: https://dev.twitter.com/rest/reference/get/search/tweets
 
-    Twitter.get('search/tweets', params, function(err, data) {
-            //Declare Variables
-            var ogText = '';
-            var retweetID = '';
-            var name = '';
-            var i = 0;
-            // if there no errors
-            if (!err) {
-                for (i; i<5; i++){
-                    ogText = data.statuses[i].text;
-                    retweetID = data.statuses[i].id_str;
-                    name = data.statuses[i].user.screen_name;
-                    if(ogText.length <= 105){
-                        if (ogText.length>=60)
-                            break;
-                    }
-                }
-                //Remove non alpha-numeric chars from tweet to save space 
-                //Russian translation increases character count
-                ogText = ogText.replace(/[,\!$%\^&\*;{}=\-_`~()]/g,"");
-                ogText = ogText.replace("\n","");
-                //Set Translation parameters
-                var params = {
-                  text: ogText,
-                  from: 'en',
-                  to: 'ru'
-                };
+    Twitter.get('search/tweets', params, function(err, data, response) {
+        // if there no errors
+        if (!err) {
+            
+            ogText = data.statuses[0].text;
+            retweetID = data.statuses[0].id_str;
+            name = data.statuses[0].user.screen_name;
+                
+            //Remove non alpha-numeric chars from tweet to save space 
+            //Russian translation increases character count
+            ogText = ogText.replace(/[,\!$%\^&\*;{}=\-_`~()]/g,"");
+            ogText = ogText.replace("\n","");
+            //Set Translation parameters
+            var params = {
+              text: ogText,
+              from: 'en',
+              to: 'ru'
+            };
 
-                // Translate text of original tweet
-                client.translate(params, function(err, data) {             
-                    var twitText = JSON.stringify(data);
-                    if (twitText.length > 140) {
-                        var diff = twitText.length - 140;
-                        twitText = twitText.substring(0, twitText.length - diff);
+            // Translate text of original tweet
+            client.translate(params, function(err, data) {             
+                var twitText = JSON.stringify(data);
+                
+                // Russian text seems to add characters, this loop ensures
+                // total text length < 140 after translation/adding chars
+                if (twitText.length > 108) {
+                    while(twitText.length > 108){
+                        var index = twitText.lastIndexOf(' ');
+                        twitText = twitText.slice(0,index);
                     }
                     
                     // Tell Twitter to reply to User with translated tweet
                     Twitter.post('statuses/update', {in_reply_to_status_id: retweetID, 
-                        status: 'Translation: ' + twitText + ' @' + name}, function(err, response) {
-                        if (response) {
-                            console.log('Retweeted!!!');
+                    status: 'Translation: ' + twitText + '" @' + name}, function(err, data, response) {
+                        if (!err) {
+                            console.log('Successfully tweeted with string truncation!!!');
+                            console.log(timestamp());
                         }
                         // if there was an error while tweeting
-                        if (err) {
-                            console.log(err);
+                        else{
+                            console.log('No new tweets\n');
+                            console.log(timestamp());
+                            console.log('\nError Message : ' + err.message, '\nCode: '+err.code);
+                            console.log('\n\n');
                         }
                     });
-                });
-            }
-            // if unable to Search a tweet
-            else {
-              console.log('Something went wrong while SEARCHING...');
-            }
-        });
+
+                    var diff = twitText.length - 140;
+                    twitText = twitText.substring(0, twitText.length - diff);
+                }
+                else{
+                    // Tell Twitter to reply to User with translated tweet
+                    Twitter.post('statuses/update', {in_reply_to_status_id: retweetID, 
+                    status: 'Translation: ' + twitText + ' @' + name}, function(err, data, response) {
+                        if (!err) {
+                            console.log('Successfully tweeted!!!');
+                            console.log(timestamp());
+                        }
+                        // if there was an error while tweeting
+                        else{
+                            console.log('No new tweets\n');
+                            console.log(timestamp());
+                            console.log('\nError Message : ' + err.message, '\nCode: '+err.code);
+                            console.log('\n\n');
+                        }
+                    });
+
+                }
+            });
+        }
+        // if unable to Search a tweet
+        else {
+            console.log('Something went wrong while SEARCHING...\n');
+            console.log(timestamp());
+            console.log('\nError Message : ' + err.message, '\nCode: '+err.code);
+            console.log('\n\n');
+        }
     });
+});
